69b2ea344a037e4566a7c771d46351f6f87c58ea
Jarek Chechelski <chechelskii@gmail.com>
2017-04-19 15:51:16 +0200

    js file is ready

---
+++ www/js/script.js
@@ -0,0 +1,14 @@
+(function() {
+    window.onload = function() {
+        init();
+    }
+})();
+
+function init() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+
+function onDeviceReady() {
+    // code
+}
