7e04ccecc3456ff5596eef688711f7594bd08fff
Jarek Chechelski <chechelskii@gmail.com>
2017-04-19 17:00:19 +0200

    check interent connection

--- www/js/script.js
+++ www/js/script.js
@@ -10,5 +10,26 @@ function init() {
 
 
 function onDeviceReady() {
-    // code
+    
+    checkConnection();
+    
+    
+}
+
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+    
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+    if(networkState == Connection.NONE) {
+        navigator.notification.alert(states[networkState].toUpperCase() + '\n You can not track parcels');
+    }
 }
